4fff155fd73c837b0e66bdf7492151e7023f4c36
linzhenghao233 <1392872316@qq.com>
2023-09-20 21:25:26 +0800

    0920

--- list.h
+++ list.h
@@ -21,3 +21,19 @@ typedef struct node {
 }Node;
 
 typedef Node* List;
+
+/* 函数原型 */
+
+/* 操作：初始化一个链表 */
+/* 前提条件：plist指向一个链表 */
+/* 后置条件：链表初始化为空 */
+void InitializeList(List* plist);
+
+/* 操作：确定链表是否为空定义，plist指向一个已初始化的链表 */
+/* 后置条件：如果链表为空，该函数返回true；否则返回false*/
+bool ListIsFull(const List* plist);
+
+/* 操作：确定链表中的项数，plist指向一个已初始化的链表 */
+/* 后置条件：该函数返回链表中的项数 */
+unsigned int ListItemCount(const List* plist);
+
