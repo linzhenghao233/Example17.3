1603a4654f584e76706147120b9d071e7883590e
linzhenghao233 <1392872316@qq.com>
2023-09-21 21:34:21 +0800

    0921

--- list.h
+++ list.h
@@ -37,3 +37,20 @@ bool ListIsFull(const List* plist);
 /* 后置条件：该函数返回链表中的项数 */
 unsigned int ListItemCount(const List* plist);
 
+/* 操作：在链表的末尾添加项 */
+/* 前提条件：item是一个待添加至链表的项，plist指向一个已初始化的链表 */
+/* 后置条件：如果可以，该函数在链表末尾添加一个项，且返回true；否则返回false */
+bool AddItem(Item item, List* plist);
+
+/* 操作：把函数作用于链表中的每一项 */
+/*          plist指向一个已初始化的链表 */
+/*          pfun指向一个函数，该函数接收一个Item类型的参数，且无返回值 */
+/*后置条件：pfun指向的函数作用于链表中的每一项一次 */
+void Traverse(const List* plist, void(*pfun)(Item item));
+
+/* 操作：释放已分配的内存（如果有的话）*/
+/*          plist指向一个已初始化的链表 */
+/* 后置条件：释放了为链表分配的所有内存，链表设置为空 */
+void EmptyTheList(List* plist);
+
+#endif
